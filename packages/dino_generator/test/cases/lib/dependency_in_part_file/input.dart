ab229db845a23c9da9a5a669b864c8425a1a742c
Exeteres <exeteres@yandex.ru>
2022-08-09 21:33:58 +0700

    chore: Fix invalid import

--- packages/dino_generator/test/cases/lib/dependency_in_part_file/input.dart
+++ packages/dino_generator/test/cases/lib/dependency_in_part_file/input.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:dino/dino.dart';
 
-import 'package:generator_test_cases/duplicate_registrations/output.dart';
+import 'package:generator_test_cases/dependency_in_part_file/output.dart';
 
 void main() {
   final ServiceCollection services = $ServiceCollection();
